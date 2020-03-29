33ba4f6606ec78658b461577d44edd64a7973522
LiuYongMing <1659269348@qq.com>
2020-03-29 20:11:57 +0800

    init commit

---
+++ src/main/java/com/atguigu/github/Master.java
@@ -0,0 +1,12 @@
+package com.atguigu.github;
+
+/**
+ * @Author: PC
+ * @Date: 2020-03-29 19:37
+ * @Description:  测试用IDEA提交项目到GitHub
+ */
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("I am master");
+    }
+}
