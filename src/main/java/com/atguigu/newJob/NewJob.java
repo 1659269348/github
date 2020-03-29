f05344f1bc2b70b67139f982a87dba0f2fffc3c0
LiuYongMing <2413081442 @qq.com>
2020-03-29 22:57:21 +0800

    new job

---
+++ src/main/java/com/atguigu/newJob/NewJob.java
@@ -0,0 +1,12 @@
+package com.atguigu.newJob;
+
+/**
+ * @Author: PC
+ * @Date: 2020-03-29 22:55
+ * @Description:
+ */
+public class NewJob {
+    public static void main(String[] args) {
+        System.out.println("新工作要来了");
+    }
+}
